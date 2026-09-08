24a5d1858863d376c8de86f54f3e3cf77f9c9b31
Pedro Castro <vieiracastropedro7@gmail.com>
2026-09-08 19:16:10 -0300

    feat: adiciona tema claro e novo botão para mudança do tema

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,38 @@
 import { Link } from "react-router-dom";
 
+import type { ThemeTypes } from "../../types/theme";
+import { Logo } from "../../assets/logo";
 import styles from "./header.module.css";
-import logo from "../../assets/logo.svg";
+import { BsMoon, BsSun } from "react-icons/bs";
 
-export function Header() {
+interface HeaderProps {
+  theme: ThemeTypes;
+  onToggleTheme: () => void;
+}
+
+export function Header({ theme, onToggleTheme }: HeaderProps) {
   return (
     <header className={styles.container}>
       <Link to="/">
-        <img src={logo} alt="Logo DevCurrency" />
+        <Logo className={styles.logo} />
       </Link>
 
-      <nav className={styles.nav}>
-        <Link to="/sobre">Sobre</Link>
-      </nav>
+      <div className={styles.sider}>
+        <nav className={styles.nav}>
+          <Link to="/sobre">Sobre</Link>
+        </nav>
+
+        <button
+          type="button"
+          className={styles.themeButton}
+          onClick={onToggleTheme}
+          aria-label="Alternar tema"
+        >
+          <span key={theme} className={styles.iconWrapper}>
+            {theme === "light" ? <BsMoon size={28} /> : <BsSun size={28} />}
+          </span>
+        </button>
+      </div>
     </header>
   );
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../header";
+import type { ThemeTypes } from "../../types/theme";
 
 export function Layout() {
+  const [theme, setTheme] = useState<ThemeTypes>(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark" || savedTheme === "light"
+      ? savedTheme
+      : "light";
+  });
+
+  function toggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <>
-      <Header />
+      <Header theme={theme} onToggleTheme={toggleTheme} />
       <Outlet />
     </>
   );

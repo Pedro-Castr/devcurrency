e5f02a2fdddc9f8efdc9ef283e9229c696b693f5
Pedro Castro <vieiracastropedro7@gmail.com>
2026-09-24 18:49:56 -0300

    refactor: altera nomenclatura dos testes de formatação

--- src/utils/formatCurrency.test.ts
+++ src/utils/formatCurrency.test.ts
@@ -8,24 +8,32 @@ import {
   formatPercent,
 } from "./formatCurrency";
 
-describe("Testes de Formatação", () => {
-  test("FormatCurrency de 18.54 deve retornar R$ 18,54", () => {
+describe("formatCurrency", () => {
+  test("formata um valor decimal como moeda em reais", () => {
     expect(formatCurrency(18.54)).toBe("R$\u00A018,54");
   });
+});
 
-  test("formatCompactCurrency de 1010030045 deve retornar R$ 1,01 bi", () => {
+describe("formatCompactCurrency", () => {
+  test("formata valores na casa dos bilhões de forma abreviada", () => {
     expect(formatCompactCurrency(1010030045)).toBe("R$\u00A01,01\u00a0bi");
   });
+});
 
-  test("formatNumber de 1226.59 deve retornar 1.226,59", () => {
+describe("formatNumber", () => {
+  test("formata um número decimal com separador de milhar no padrão brasileiro", () => {
     expect(formatNumber(1226.59)).toBe("1.226,59");
   });
+});
 
-  test("formatCompactNumber de 3348122.62 deve retornar 3,35 mi", () => {
+describe("formatCompactNumber", () => {
+  test("formata valores na casa dos milhões de forma abreviada", () => {
     expect(formatCompactNumber(3348122.62)).toBe("3,35\u00a0mi");
   });
+});
 
-  test("formatPercent de 93.6 deve retornar 93,60%", () => {
+describe("formatPercent", () => {
+  test("formata um número como porcentagem com duas casas decimais", () => {
     expect(formatPercent(93.6)).toBe("93,60%");
   });
 });
